9b0f7464f7fcf49d0298bd7b3ff5907c134c7dfc
minzinphyo <minzinphyo143@gmail.com>
2024-06-08 22:21:21 +0900

    Initial Commit

---
+++ noteapp/noteappApp.swift
@@ -0,0 +1,17 @@
+//
+//  noteappApp.swift
+//  noteapp
+//
+//  Created by Min Zin Phyo on 08/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct noteappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
